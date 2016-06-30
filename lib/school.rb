1c39f732351d359e9e5a2fd876e356aa90978830
FrantzLacarriere <frantz.lacarriere@gmail.com>
2016-06-30 14:46:32 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -7,12 +7,8 @@ class School
   end
 
   def add_student(name, grade)
-    if (@roster[grade].is_a?(Array))
-      @roster[grade] << name
-    else
-      @roster[grade] = []
-      @roster[grade] << name
-    end
+    @roster[grade] ||= []
+    @roster[grade] << name
   end
 
   def grade(grade)
